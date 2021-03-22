754e4f8f3e7ca433cbe8b860375e193f329257e6
Stepan Kosatyi <stepan@kosatyi.com>
2021-03-22 16:23:18 +0200

    update

--- src/core/component.js
+++ src/core/component.js
@@ -1,6 +1,6 @@
 var waterfall  = require('./waterfall');
 var x64hash128 = require('./x64hash128');
-var Base64     = require('./Base64');
+var Base64     = require('./base64');
 /**
  *
  * @constructor
